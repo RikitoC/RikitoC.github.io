f96218843bc0433327ebdf99559d2fc6698b90a1
RikitoC <rickyspetgila@gmail.com>
2026-03-28 19:42:23 -0500

    Update pipeline.py

--- scraper/pipeline.py
+++ scraper/pipeline.py
@@ -3,9 +3,9 @@ import os
 
 from scraper.stages.crews import run as run_crews
 from scraper.stages.crew_details import run as run_crew_details
+from scraper.stages.external import run as run_external
 from scraper.stages.pirate_urls import run as run_pirate_urls
 from scraper.stages.pirates import run as run_pirates
-from scraper.stages.external import run as run_external
 from scraper.stages.shoppes import run as run_shoppes
 from scraper.stages.finalize import run as run_finalize
 
